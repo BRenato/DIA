308a00f907b74ffd15624324477aa158a85b2843
Nunalves <nunalves@outlook.com>
2015-01-24 12:54:45 +0000

    Constructors methods
    add methods

--- js/logic/constructors.js
+++ js/logic/constructors.js
@@ -9,7 +9,6 @@
 *	Issue
 */
 
-
 // This is the one to use most commonly when creating a new issue
 function Issue(owner, title, description, type, category){
 	this.owner = owner;
@@ -18,17 +17,17 @@ function Issue(owner, title, description, type, category){
 	this.type = type;
 	this.category = category;
 	this.creationDate = Date.now();
-	this.status = 'Initial';
+	this.status = 'I';
 	this.likes = 0;
 	this.dislikes = 0;
 	this.workgroup = null;
 }
 
-Issue.prototype.nextStatus = function(){
-	var possibleStatus = ['I', 'A', 'O', 'F'];
+// Issue.prototype.nextStatus = function(){
+// 	var possibleStatus = ['I', 'A', 'O', 'F'];
 
-	this.status = possibleStatus[possibleStatus.indexOf(this.status) + 1];
-};
+// 	this.status = possibleStatus[(possibleStatus.indexOf(this.status) + 1)];
+// };
 
 Issue.prototype.incrementLikes = function(){
 	this.likes += 1;
@@ -38,39 +37,98 @@ Issue.prototype.incrementDislikes = function(){
 	this.dislikes += 1;
 };
 
+Issue.prototype.setWorkgroup = function(workgroup){
+	this.workgroup = workgroup;
+};
 
 /**********************************************************
 *	WorkGroup
 */
 
-// Methods to use
 function WorkGroup(){
+	//the duration of the task to be performed
 	this.duration = null;
+	
+	//the date chosen by the Issue Owner between all possible dates
 	this.executionDate = null;
+	
+	//all the possible dates available for the users to chose from, must be setted by the issue owner
 	this.possibleDates = [];
+	
+	//the users that have previously volunteered on the Issue
 	this.volunteers = [];
+	
+	//the users that the Issue owner wants to invite
+	this.invitedVolunteers = [];
+	
+	//the materials provided by the users and accepted by the issue owner
 	this.toolsMaterials = [];
 }
 
-function WorkGroup(duration, executionDate, possibleDates, volunteers, toolsMaterials){
+// sets the duration of the task to be performed
+WorkGroup.prototype.setDuration = function(duration){
 	this.duration = duration;
+};
+
+// sets the chosen execution date of the task to be performed 
+WorkGroup.prototype.setExecutionDate = function(executionDate){
 	this.executionDate = executionDate;
-	this.possibleDates = possibleDates;
-	this.volunteers = volunteers;
-	this.toolsMaterials = toolsMaterials;
-}
+};
 
-// WorkGroup.prototype.addPossibleDate = function(date){
-// 	this.possibleDates.push(date);
-// };
+// adds a volunteer to the volunteers list (this are the previously volunteered, NOT the invited ones)
+// returns false if the user is already on the volunteers or invitedVolunteers list
+WorkGroup.prototype.addVolunteer = function(volunteer){
+	var alreadyVolunteer_bool = (this.volunteers.indexOf(volunteer) > -1) || (this.invitedVolunteers.indexOf(volunteer) > -1)
+	if (!alreadyVolunteer_bool){
+		this.volunteers.push(volunteer);
+	}
+	return !alreadyVolunteer_bool;
+};
 
-// WorkGroup.prototype.addVolunteer = function(volunteer){
-// 	this.volunteers.push(volunteer);
-// };
+// removes a volunteer from the volunteers list (we must decide if the issue owner can remove this volunteers or not)
+// returns false if volunteer not found, otherwise returns the removed volunteer
+WorkGroup.prototype.removeVolunteer = function(volunteer){
+	var volunteerIndex = this.volunteers.indexOf(volunteer);
+	if (volunteerIndex > -1) {
+    	return this.volunteers.splice(volunteerIndex, 1);
+	}
+	else{
+		return false;
+	}
+};
 
-// WorkGroup.prototype.addToolsMaterials = function(toolMaterial){
-// 	this.toolsMaterials.push(toolMaterial);
-//};
+// adds a volunteer to the invited volunteers list (users that the issue owner wants to invite)
+// returns false if the user is already on the volunteers or invitedVolunteers list
+WorkGroup.prototype.addInvitedVolunteer = function(invitedVolunteer){
+	var alreadyVolunteer_bool = (this.volunteers.indexOf(invitedVolunteer) > -1) || (this.invitedVolunteers.indexOf(invitedVolunteer) > -1)
+	if (!alreadyVolunteer_bool){
+		this.invitedVolunteers.push(invitedVolunteer);
+	}
+	return !alreadyVolunteer_bool;
+};
+
+// removes a volunteer from the invited volunteers list (users that the issue owner wants to invite)
+// eturns false if volunteer not found, otherwise returns the removed volunteer
+WorkGroup.prototype.removeInvitedVolunteer = function(invitedVolunteer){
+	var volunteerIndex = this.invitedVolunteers.indexOf(invitedVolunteer);
+	if (volunteerIndex > -1) {
+    	return this.invitedVolunteers.splice(volunteerIndex, 1);
+	}
+	else{
+		return false;
+	}
+};
+
+// add a tool or material to the toolsMaterial list
+WorkGroup.prototype.addToolMaterial = function(toolMaterial){
+	this.toolsMaterials.push(toolMaterial);
+};
+
+// remove a tool or material to the toolsMaterial list
+WorkGroup.prototype.addToolMaterial = function(toolMaterial){
+	this.toolsMaterials.push(toolMaterial);
+};
+
 
 /*********************************************************
 *	ToolMaterial
@@ -100,16 +158,11 @@ function User(){
 /*********************************************************
 *	Volunteer
 */
-function Volunteer(){
-	this.user = {};
+
+function Volunteer(user){
+	this.user = user;
 	this.toolsMaterials = [];
 	this.possibleDates = [];
-}
-
-function Volunteer(user, toolsMaterials, possibleDates){
-	this.user = user;
-	this.toolsMaterials = toolsMaterials;
-	this.possibleDates = possibleDates;
 }
 
 // Volunteer.prototype.addPossibleDate = function(date){
